90c779c26dc90cda7f01295bd8fee6edf8ec75b0
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2022-06-19 11:20:39 +0530

    Update linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -5,13 +5,15 @@ outt() {
 }
 
 install_env(){
+clear
 echo -e " Setting up Env "
 yes|outt pkg upgrade
 outt apt-get update -y && outt apt install -y wget proot
 wget -q https://github.com/anonymousx97/ubuntu/raw/master/ubuntu.sh
 outt chmod +x ubuntu.sh &&  outt ./ubuntu.sh -y
-echo -e " Done, Starting Ubuntu with ./startubuntu.sh. "
-startubuntu.sh
+echo -e " 
+Done, Starting Ubuntu with ./startubuntu.sh. "
+chmod +x startubuntu.sh && ./startubuntu.sh
 }
 
 install_env 2> /dev/null
