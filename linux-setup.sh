7fbf8f107ad2879e4bbf3dfc4a7a0aa63063fb90
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2022-06-19 11:17:47 +0530

    Update linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -11,7 +11,7 @@ outt apt-get update -y && outt apt install -y wget proot
 wget -q https://github.com/anonymousx97/ubuntu/raw/master/ubuntu.sh
 outt chmod +x ubuntu.sh &&  outt ./ubuntu.sh -y
 echo -e " Done, Starting Ubuntu with ./startubuntu.sh. "
-bash startubuntu.sh
+startubuntu.sh
 }
 
 install_env 2> /dev/null
