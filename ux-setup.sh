5015193eff8ce1c6713d5f71c9b11cf84c768f2a
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2022-06-19 11:44:30 +0530

    Update ux-setup.sh

--- ux-setup.sh
+++ ux-setup.sh
@@ -12,24 +12,24 @@ echo -e "
        1.Installing required packages and Bot"
 
 outt "$(
-apt -qq update && apt -qq upgrade -y && \
-    apt -qq install -y --no-install-recommends \
-    apt-utils \
-    curl \
-    git \
-    gnupg2 \
-    wget \
-    unzip \
-    tree \
-gcc zlib1g-dev \
-    apt-transport-https \
-    build-essential coreutils jq pv \
-    ffmpeg mediainfo \
-    neofetch \
-    p7zip-full \
+    apt -qq update && apt -qq upgrade -y
+    yes|apt install
+    apt-utils
+    curl
+    git
+    gnupg2
+    wget
+    unzip
+    tree
+    gcc zlib1g-dev
+    apt-transport-https
+    build-essential coreutils jq pv
+    ffmpeg mediainfo
+    neofetch
+    p7zip-full
     libfreetype6-dev libjpeg-dev libpng-dev libgif-dev libwebp-dev
-apt install -y python3 python-dev-is-python3 python3-distutils libxslt-dev libxml2 nano
-apt install pip -y)" 2> /dev/null
+    yes|apt install python3 python-dev-is-python3 python3-distutils libxslt-dev libxml2 nano
+    yes|apt install pip)" 2> /dev/null
 echo -e "   
          Packages are set.  "
 echo -e "
