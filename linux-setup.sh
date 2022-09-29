03ec7c6a070ab3bb3657d0fff875421ab0f61467
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2022-09-29 19:14:35 +0530

    Update linux-setup.sh

--- linux-setup.sh
+++ linux-setup.sh
@@ -9,8 +9,7 @@ outt() {
 install_env(){
 clear
 echo -e "       Setting up Env "
-yes|outt pkg upgrade
-outt apt-get update -y && outt apt install -y wget proot
+outt apt update -y && outt apt install -y wget proot
 wget -q https://raw.githubusercontent.com/ux-termux/ux-termux/main/ubuntu.sh
 outt chmod +x ubuntu.sh &&  outt ./ubuntu.sh -y
 echo -e "    
