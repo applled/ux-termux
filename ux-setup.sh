47fea64d1edff36b8cffa85d76565c353339beca
Ryuk <88324835+anonymousx97@users.noreply.github.com>
2022-09-30 08:38:44 +0530

    Update ux-setup.sh

--- ux-setup.sh
+++ ux-setup.sh
@@ -2,11 +2,9 @@
 
 install_ux(){
 clear
-echo -e "
-       1.Installing required packages for Bot
-         this may take a while."
-echo -e "
-           Spam incoming in 5"
+echo -e "\n • 1.Installing required packages for Bot
+  this may take a while."
+echo -e "\n\n *** Spam incoming in 5 ***"
 sleep 6
 
 yes|apt install\
@@ -28,21 +26,15 @@ yes|apt install python3 python-dev-is-python3 python3-distutils libxslt-dev libx
 yes|apt install pip
 pip install -U pip
 pip install -U setuptools
-
-echo -e "   
-         Packages are set.  "
-echo -e "
-       2.Clonning Repo."
+clear
+echo -e "\n • 2.Clonning Repo."
 
 git clone -q https://github.com/ux-termux/UX-jutsu ux
 cd ux
-echo -e "
-       3.Done, Now Installing requirements,
-        this may take a while, wait patiently."
-pip install --no-cache-dir -r requirements.txt
-clear
-echo -e "
-       Follow the next step in guide."
+echo -e "\n • 3.Done, Now Installing requirements,
+  this may take a while, wait patiently."
+pip install -q --no-cache-dir -r requirements.txt
+echo -e "\n • Follow the next step in guide."
 }
 
 install_ux 2> /dev/null
